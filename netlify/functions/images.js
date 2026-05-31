52385a25ebd74d9ca285c41ed5cd42da885ecbd0
jemmavonhoff <jemmavonhoff@gmail.com>
2026-06-01 09:35:23 +1000

    Update images.js

--- netlify/functions/images.js
+++ netlify/functions/images.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 
+exports.handler = async function(event, context) {
+  try {
+    const imagesDir = path.join(__dirname, '../../images');
+    const files = fs.readdirSync(imagesDir);
+    const images = files.filter(f =>
+      /\.(jpg|jpeg|png|gif|webp)$/i.test(f) && f !== '.gitkeep'
+    );
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(images)
+    };
+  } catch (e) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify([])
+    };
+  }
+};
